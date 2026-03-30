37f25e83c43ea9242d5d47335c7aa7aea45024b7
Duncan Holmes <duncancholmes@gmail.com>
2026-03-30 16:20:09 -0500

    get rid of unused C file

---
+++ cmake-build-debug/CMakeFiles/322_programming_hmk_1_translation_and_validaiton.dir/main.c.obj.d
@@ -0,0 +1,13 @@
+CMakeFiles/322_programming_hmk_1_translation_and_validaiton.dir/main.c.obj: \
+ C:\Users\dunca\Desktop\code_files\CSCE\CSCE_322_AKA_programming_language_concepts_AKA_mr_t_class\322_Clion_super_folder\322_programming_assignments\main.c \
+ C:/Users/dunca/AppData/Local/Programs/CLion/bin/mingw/x86_64-w64-mingw32/include/stdio.h \
+ C:/Users/dunca/AppData/Local/Programs/CLion/bin/mingw/x86_64-w64-mingw32/include/corecrt_stdio_config.h \
+ C:/Users/dunca/AppData/Local/Programs/CLion/bin/mingw/x86_64-w64-mingw32/include/corecrt.h \
+ C:/Users/dunca/AppData/Local/Programs/CLion/bin/mingw/x86_64-w64-mingw32/include/_mingw.h \
+ C:/Users/dunca/AppData/Local/Programs/CLion/bin/mingw/x86_64-w64-mingw32/include/_mingw_mac.h \
+ C:/Users/dunca/AppData/Local/Programs/CLion/bin/mingw/x86_64-w64-mingw32/include/_mingw_secapi.h \
+ C:/Users/dunca/AppData/Local/Programs/CLion/bin/mingw/x86_64-w64-mingw32/include/vadefs.h \
+ C:/Users/dunca/AppData/Local/Programs/CLion/bin/mingw/x86_64-w64-mingw32/include/sdks/_mingw_ddk.h \
+ C:/Users/dunca/AppData/Local/Programs/CLion/bin/mingw/x86_64-w64-mingw32/include/_mingw_off_t.h \
+ C:/Users/dunca/AppData/Local/Programs/CLion/bin/mingw/x86_64-w64-mingw32/include/swprintf.inl \
+ C:/Users/dunca/AppData/Local/Programs/CLion/bin/mingw/x86_64-w64-mingw32/include/sec_api/stdio_s.h
--- main.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main(void)
-{
-    printf("Hello me, meet the real me.\n");
-    return 0;
-}
