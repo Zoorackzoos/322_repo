58c3f315e3ef78230d3af9b865f3baaebb1e09d8
Zoorackzoos <duncancholmes@gmail.com>
2026-05-03 05:47:30 -0500

    pa4: meme try.

--- programming/pa4/run-triple-t.rkt
+++ programming/pa4/run-triple-t.rkt
@@ -2,8 +2,29 @@
 (require "pa1.rkt")
 (require "pa4.rkt")
 
+;;meme program "variable"
+(define tung-art "    __|__\n   / o o \\\n  |  ---  |\n  |  TTT  |\n   \\_____/\n  SAHUR!!")
+
 (define args (current-command-line-arguments))
 (define prog (call-with-input-file (vector-ref args 0) read))
-(define result (evaluate-with-env prog '()))
+
+;;meme program input detector
+(define (contains-tung? e)
+  (cond
+    [(equal? e 'tung) #t]
+    [(equal? e 'sahur) #t]
+    [(list? e) (ormap contains-tung? e)]
+    [else #f]
+  )
+)
+
+(define result
+  (if
+    (contains-tung? prog) ;;conditional
+    (begin (display tung-art) (newline) 'tung-tung-tung-sahur) ;;if yes
+    (evaluate-with-env prog '()) ;;if no
+  )
+)
+
 (display result)
 (newline)
